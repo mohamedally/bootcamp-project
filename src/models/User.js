06694eb5e7cdd0e67f685f2cb8d2b2c1d7108c44
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-01-15 15:55:52 -0500

    Added migrations and models

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,48 @@
 const BaseModel = require('./BaseModel')
+const { HasManyRelation, ManyToManyRelation } = require('objection')
 
 class User extends BaseModel {
   static get tableName() {
     return 'users'
+  }
+
+  static get relationMappings() {
+    const Post = require('./Post')
+
+    return {
+      posts: {
+        relation: HasManyRelation,
+        modelClass: Post,
+        join: {
+          from: 'users.id',
+          to: 'posts.userId',
+        },
+      },
+      friends: {
+        relation: ManyToManyRelation,
+        modelClass: User,
+        join: {
+          from: 'users.id',
+          through: {
+            from: 'friends.userOne',
+            to: 'friends.userTwo',
+          },
+          to: 'users.id',
+        },
+      },
+      friend_requests: {
+        relation: ManyToManyRelation,
+        modelClass: User,
+        join: {
+          from: 'users.id',
+          through: {
+            from: 'friend_requests.sender',
+            to: 'friend_requests.receiver',
+          },
+          to: 'users.id',
+        },
+      },
+    }
   }
 }
 
